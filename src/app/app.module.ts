b58456ee76c9e04df20d387eb26ffd2593249cd7
damian spizzirri <damianspizzirri@gmail.com>
2022-04-10 20:07:25 -0300

    Data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { TaskModule } from './components/task.module';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+
 import { AppComponent } from './app.component';
-import { TaskComponent } from './components/task/task.component';
-import { TaskListComponent } from './components/task-list/task-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TaskComponent,
-    TaskListComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TaskModule,
+NgxsModule.forRoot([]),
+NgxsReduxDevtoolsPluginModule.forRoot(),
+NgxsLoggerPluginModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
